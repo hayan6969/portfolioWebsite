c11c87c55c94af908fc7fba6471f822b89168e26
Hayan Khan <hayankhanmehsood@gmail.com>
2023-10-14 01:07:40 +0500

    cv new

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Cv() {
   return (
     <div className='p-6  flex justify-center items-center'>
-      <a href="https://drive.google.com/file/d/1y-cNEg_6pXY3m5okCXyBGHYSlGlqaAHz/view?usp=sharing"  target='_blank'>
+      <a href="https://drive.google.com/file/d/1rwEoT4wCUNsnoAOJrMD_3bYABmMSFVXM/view?usp=sharing"  target='_blank'>
         <button className='text-black glassmorphism p-2 rounded-full'>Download CV</button>
       </a>
     </div>
